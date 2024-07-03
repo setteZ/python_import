840d704bcb7c8ece2be3e5484ee2f62f89099317
Pietro Zambelli <peter.zamb@gmail.com>
2024-07-03 22:17:34 +0000

    Package example

--- src/pyimp/myfamily/main.py
+++ src/pyimp/myfamily/main.py
@@ -1,37 +1,31 @@
-import sys
-from pathlib import Path
 import importlib
 
-curdir = Path(__file__).parent.absolute()
-
-print(curdir)
-sys.path.insert(0, curdir.as_posix())
-sys.path.insert(0, curdir.parent.as_posix())
-
 try:
-    from brother import bro as imp_bro
+    # from brother import bro as imp_bro
+    from pyimp.myfamily.brother import bro as imp_bro
 except Exception as e:
-    print("error: fail to import from dir")
+    print("0) error: fail to import from dir")
     print(f"{e}")
 
 try:
     from .. import fath as imp_fath
 except Exception as e:
-    print("error: fail to import from above dir")
+    print("1) error: fail to import from above dir")
     print(f"{e}")
 
-if __name__ == "__main__":
+
+def main():
     print("the main is running\n")
     print("## import_module from dir ##")
 
     try:
-        bro_module = importlib.import_module("brother.bro")
+        bro_module = importlib.import_module("pyimp.myfamily.brother.bro")
         instance = bro_module.Brother()
         instance.print()
     except:
-        print("error: fail to import_module brother.bro")
+        print("2) error: fail to import_module brother.bro")
     else:
-        print("success!")
+        print("2) success!")
     finally:
         print("")
 
@@ -40,9 +34,9 @@ if __name__ == "__main__":
         instance = imp_bro.Brother()
         instance.print()
     except:
-        print("error: fail to import_module brother.bro")
+        print("3) error: fail to import_module brother.bro")
     else:
-        print("success!")
+        print("3) success!")
     finally:
         print("")
 
@@ -52,9 +46,9 @@ if __name__ == "__main__":
         instance = fath_module.Father()
         instance.print()
     except:
-        print("error: fail 1 to import_module ..\\fath.py")
+        print("4) error: fail 1 to import_module ..\\fath.py")
     else:
-        print("success!")
+        print("4) success!")
     finally:
         print("")
 
@@ -64,18 +58,23 @@ if __name__ == "__main__":
         instance = fath_module.Father()
         instance.print()
     except:
-        print("error: fail 2 to import_module ..\\fath.py")
+        print("5) error: fail 2 to import_module ..\\fath.py")
     else:
-        print("success!")
+        print("5) success!")
     finally:
         print("")
-    
+
     print("## import from above dir ##")
 
     try:
+        from pyimp import fath as imp_fath
         module = imp_fath.Father()
         module.print()
     except:
-        print("error: fail with import fath")
+        print("6) error: fail with import fath")
     else:
-        print("success!\n")
+        print("6) success!\n")
+
+
+if __name__ == "__main__":
+    main()
